a53bd6b815ae33a081ee1b4fa6cf80d2ca8c774c
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-02-20 08:25:59 -0300

    Adicionadas Descrições do Swagger na classe PersonagemController

--- src/main/java/com/db/naruto/domain/controller/PersonagemController.java
+++ src/main/java/com/db/naruto/domain/controller/PersonagemController.java
@@ -4,6 +4,9 @@ import com.db.naruto.domain.dto.PersonagemRequest;
 import com.db.naruto.domain.dto.PersonagemResponse;
 import com.db.naruto.domain.dto.PersonagemUpdateRequest;
 import com.db.naruto.domain.service.PersonagemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +22,15 @@ public class PersonagemController {
 
     public PersonagemController(PersonagemService personagemService) {this.personagemService = personagemService;}
 
+    @Operation(description = "Lista todos os personagens criados")
+    @ApiResponse(responseCode = "200", description = "Lista todos os personagens criados")
     @GetMapping
     public ResponseEntity<List<PersonagemResponse>> listarPersonagens() {
         return ResponseEntity.ok(personagemService.listarPersonagens());
     }
 
+    @Operation(description = "Cria um ninja de ninjutsu")
+    @ApiResponse(responseCode = "200", description = "Retorna o ninja de ninjutsu criado")
     @PostMapping("/ninja_de_ninjutsu")
     public ResponseEntity<PersonagemResponse> salvarNinjaDeNinjutsu(
             @RequestBody @Valid PersonagemRequest personagemRequest
@@ -31,6 +38,8 @@ public class PersonagemController {
         return ResponseEntity.ok(personagemService.salvarNinjaDeNinjutsu(personagemRequest));
     }
 
+    @Operation(description = "Cria um ninja de taijutsu")
+    @ApiResponse(responseCode = "200", description = "Retorna o ninja de taijutsu criado")
     @PostMapping("/ninja_de_taijutsu")
     public ResponseEntity<PersonagemResponse> salvarNinjaDeTaijutsu(
             @RequestBody @Valid PersonagemRequest personagemRequest
@@ -38,12 +47,20 @@ public class PersonagemController {
         return ResponseEntity.ok(personagemService.salvarNinjaDeTaijutsu(personagemRequest));
     }
 
+    @Operation(description = "Cria um ninja de genjutsu")
+    @ApiResponse(responseCode = "200", description = "Retorna o ninja de genjutsu criado")
     @PostMapping("/ninja_de_genjutsu")
     public ResponseEntity<PersonagemResponse> salvarNinjaDeGejutsu(
             @RequestBody @Valid PersonagemRequest personagemRequest
     ){
         return ResponseEntity.ok(personagemService.salvarNinjaDeGenjutsu(personagemRequest));
     }
+
+    @Operation(description = "Deleta um personagem com base no id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma resposta vazia quando deleta o personagem com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Não encontrou o personagem pelo id informado")
+    })
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarPersonagem(
@@ -53,6 +70,11 @@ public class PersonagemController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Atualiza um personagem com base no id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o personagem atualizado"),
+            @ApiResponse(responseCode = "500", description = "Não encontrou o personagem pelo id informado")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<PersonagemResponse> atualizarPersonagem(
             @PathVariable Long id,
