3b5e4e1a3bc5ebb3a58853fdd42d2cf20ff27275
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-03-04 18:51:37 -0300

    test: refatorando testes de integração para funcionar com o map de jutsus

--- src/test/java/com/db/naruto/domain/service/PersonagemIntegrationTest.java
+++ src/test/java/com/db/naruto/domain/service/PersonagemIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.hasKey;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -38,19 +39,28 @@ public class PersonagemIntegrationTest {
         String json = """
             {
               "nome": "Sakura Haruno",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Contra-Genjutsu"],
-              "chakra": 65
+              "vida": 115,
+              "tipoNinja": "NINJUTSU",
+              "jutsus": {
+                "Contra-Genjutsu": {
+                    "dano": 5,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
-        mockMvc.perform(post("/personagem/ninja_de_ninjutsu")
+        mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.nome").value("Sakura Haruno"));
+                .andExpect(jsonPath("$.nome").value("Sakura Haruno"))
+                .andExpect(jsonPath("$.vida").value(115))
+                .andExpect(jsonPath("$.jutsus", hasKey("Contra-Genjutsu")))
+                .andExpect(jsonPath("$.jutsus['Contra-Genjutsu'].dano").value(5))
+                .andExpect(jsonPath("$.jutsus['Contra-Genjutsu'].consumoDeChakra").value(10));
+
     }
 
     @Test
@@ -58,20 +68,28 @@ public class PersonagemIntegrationTest {
     void deveCriarNinjaDeGenjutsuComSucesso() throws Exception {
         String json = """
             {
-              "nome": "Shikamaru Nara",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Método das Sombras"],
-              "chakra": 65
+              "nome": "Ino Yamanaka",
+              "vida": 115,
+              "tipoNinja": "GENJUTSU",
+              "jutsus": {
+                "Técnica de Transferência de Mente": {
+                    "dano": 25,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
-        mockMvc.perform(post("/personagem/ninja_de_genjutsu")
+        mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.nome").value("Shikamaru Nara"));
+                .andExpect(jsonPath("$.nome").value("Ino Yamanaka"))
+                .andExpect(jsonPath("$.vida").value(115))
+                .andExpect(jsonPath("$.jutsus", hasKey("Técnica de Transferência de Mente")))
+                .andExpect(jsonPath("$.jutsus['Técnica de Transferência de Mente'].dano").value(25))
+                .andExpect(jsonPath("$.jutsus['Técnica de Transferência de Mente'].consumoDeChakra").value(10));
 
     }
 
@@ -81,19 +99,27 @@ public class PersonagemIntegrationTest {
         String json = """
             {
               "nome": "Neji Hyuuga",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Oito Trigramas: Sessenta e Quatro Palmas"],
-              "chakra": 45
+              "vida": 130,
+              "tipoNinja": "TAIJUTSU",
+              "jutsus": {
+                "Oito Trigramas: Palma Rotativa": {
+                    "dano": 35,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
-        mockMvc.perform(post("/personagem/ninja_de_taijutsu")
+        mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.nome").value("Neji Hyuuga"));
+                .andExpect(jsonPath("$.nome").value("Neji Hyuuga"))
+                .andExpect(jsonPath("$.vida").value(130))
+                .andExpect(jsonPath("$.jutsus", hasKey("Oito Trigramas: Palma Rotativa")))
+                .andExpect(jsonPath("$.jutsus['Oito Trigramas: Palma Rotativa'].dano").value(35))
+                .andExpect(jsonPath("$.jutsus['Oito Trigramas: Palma Rotativa'].consumoDeChakra").value(10));
 
     }
 
@@ -104,44 +130,56 @@ public class PersonagemIntegrationTest {
         String json1 = """
             {
               "nome": "Sakura Haruno",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Contra-Genjutsu"],
-              "chakra": 65
+              "vida": 115,
+              "tipoNinja": "NINJUTSU",
+              "jutsus": {
+                "Contra-Genjutsu": {
+                    "dano": 5,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
         String json2 = """
             {
-              "nome": "Shikamaru Nara",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Método das Sombras"],
-              "chakra": 65
+              "nome": "Ino Yamanaka",
+              "vida": 115,
+              "tipoNinja": "GENJUTSU",
+              "jutsus": {
+                "Técnica de Transferência de Mente": {
+                    "dano": 25,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
         String json3 = """
             {
               "nome": "Neji Hyuuga",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Oito Trigramas: Sessenta e Quatro Palmas"],
-              "chakra": 45
+              "vida": 130,
+              "tipoNinja": "TAIJUTSU",
+              "jutsus": {
+                "Oito Trigramas: Palma Rotativa": {
+                    "dano": 35,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
-        mockMvc.perform(post("/personagem/ninja_de_ninjutsu")
+        mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json1))
                 .andExpect(status().isCreated());
 
-        mockMvc.perform(post("/personagem/ninja_de_genjutsu")
+        mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json2))
                 .andExpect(status().isCreated());
 
-        mockMvc.perform(post("/personagem/ninja_de_taijutsu")
+        mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json3))
                 .andExpect(status().isCreated());
@@ -151,7 +189,7 @@ public class PersonagemIntegrationTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.length()").value(3))
                 .andExpect(jsonPath("$[0].nome").value("Sakura Haruno"))
-                .andExpect(jsonPath("$[1].nome").value("Shikamaru Nara"))
+                .andExpect(jsonPath("$[1].nome").value("Ino Yamanaka"))
                 .andExpect(jsonPath("$[2].nome").value("Neji Hyuuga"));
     }
 
@@ -162,14 +200,18 @@ public class PersonagemIntegrationTest {
         String json = """
             {
               "nome": "Sakura Haruno",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Contra-Genjutsu"],
-              "chakra": 65
+              "vida": 115,
+              "tipoNinja": "NINJUTSU",
+              "jutsus": {
+                "Contra-Genjutsu": {
+                    "dano": 5,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
-        String response = mockMvc.perform(post("/personagem/ninja_de_ninjutsu")
+        String response = mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
@@ -195,14 +237,18 @@ public class PersonagemIntegrationTest {
         String json = """
             {
               "nome": "Sakura",
-              "idade": 12,
-              "aldeia": "Konoha",
-              "jutsus": ["Contra-Genjutsu"],
-              "chakra": 65
+              "vida": 115,
+              "tipoNinja": "NINJUTSU",
+              "jutsus": {
+                "Contra-Genjutsu": {
+                    "dano": 5,
+                    "consumoDeChakra": 10
+                }
+              }
             }
             """;
 
-        String response = mockMvc.perform(post("/personagem/ninja_de_ninjutsu")
+        String response = mockMvc.perform(post("/personagem")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isCreated())
@@ -225,8 +271,6 @@ public class PersonagemIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nome").value("Sakura Haruno"));
     }
-
-
 
 
 }
